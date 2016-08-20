676c769fc401b64a561fb59c0090385c687e6501
Gunvant Sarpate <sarpate007@gmail.com>
2016-08-20 12:45:01 +0530

    add interpreter.py

---
+++ interpreter.py
@@ -0,0 +1,23 @@
+import sys
+def main():
+	#input taken from user as list
+	problem=[
+		str(x) for x in input().split()
+		]
+	n=len(problem)
+	#print(n)
+	
+	#Storing values from last index of list
+	num1=(problem[n-2])
+	num2=(problem[n-1])
+	
+	#finding keywords from list
+	operation=["add","sub","mult","div"]
+	if (add in problem for add in operation):
+		add(num1,num2)
+		
+		
+def add(num1,num2):
+	ans=num1+num2
+	print("Your answer is"+ans)
+main()
